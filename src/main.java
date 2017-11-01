8d51262976c698e0a00830c90d38e67749ab0927
zch <381473354@qq.com>
2017-11-01 23:29:00 +0800

    calculate distance between two instance

--- src/main.java
+++ src/main.java
@@ -15,12 +15,12 @@ public class main {
     public static void main(String args[]) throws Exception {
         DB db=new DB("jdbc:mysql://localhost:3306/eye","root","zch123");
         Instances items=db.getData("select * from nomogram where eye='OS'");
-        Instances olditems=items;
+        Instances allitems=items;
         //属性归一化
         Normalize normalize = new Normalize();
         normalize.setInputFormat(items);
         items= Filter.useFilter(items, normalize);
-
+        //删除无用属性
         for(int i=18;i<42;i++){
             items.deleteAttributeAt(items.numAttributes()-1);
         }
@@ -30,14 +30,30 @@ public class main {
         items.deleteAttributeAt(0);
         ArrayList<Pair<Instance,Pair<Instance,Double>>> list=new ArrayList<>();
         Instance temp=items.instance(0);
-        System.out.println(items);
-        for(int i=0;i<items.numInstances();i++){
-            list.add(new Pair(olditems.instance(i),new Pair(items.instance(i),distance(temp,items.instance(i)))));
+        //计算距离bing保存到output_string中
+        StringBuffer output_string=new StringBuffer();
+        String title="";
+        for(int i=0;i<allitems.numAttributes();i++) {
+           title+=allitems.attribute(i).name()+",";
+        }
+        title+=",,distance\n";
+        for(int j=0;j<items.numInstances();j++){
+            output_string.append(title);
+            list.clear();
+        for(int i=0;i<items.numInstances();i++){//增加原属性和删除后属性的映射
+            list.add(new Pair(allitems.instance(i),new Pair(items.instance(i),distance(items.instance(j),items.instance(i)))));
         }
         sort(list);
-        for(int i=0;i<10;i++){
-            System.out.println("uuid:"+list.get(i).getKey().value(0)+"    distance:"+list.get(i).getValue().getValue());
+            output_string.append(list.get(0).getKey()+",,"+list.get(0).getValue().getValue()+"\n");
+            output_string.append("Rank:\n");
+        for(int i=1;i<11;i++){
+            output_string.append(list.get(i).getKey()+",,"+list.get(i).getValue().getValue()+"\n");
         }
+            output_string.append("\n");
+        }
+        System.out.println(output_string.toString());
+    }
+    public void write(Instances data){
 
     }
     public static double distance(Instance x,Instance y){
