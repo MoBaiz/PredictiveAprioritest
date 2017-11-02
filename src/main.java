265e650dba89695d5bf7036642da2c79d5ef806d
zch <381473354@qq.com>
2017-11-02 18:44:04 +0800

    calculate distance between two instance

--- src/main.java
+++ src/main.java
@@ -1,11 +1,12 @@
 import javafx.util.Pair;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.experiment.InstanceQuery;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Normalize;
 
-import java.time.Instant;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 /**
@@ -14,22 +15,35 @@ import java.util.ArrayList;
 public class main {
     public static void main(String args[]) throws Exception {
         DB db=new DB("jdbc:mysql://localhost:3306/eye","root","zch123");
-        Instances items=db.getData("select * from nomogram where eye='OS'");
-        Instances allitems=items;
+//        String sql="select * from nomogram where eye=0";
+//        Instances allitems=db.getData(sql);
+//        Instances items=db.getData(sql);
+//        //删除无用属性_14
+//        for(int i=18;i<42;i++){
+//            items.deleteAttributeAt(items.numAttributes()-1);
+//        }
+//        items.deleteAttributeAt(5);
+//        items.deleteAttributeAt(2);
+//        items.deleteAttributeAt(1);
+//        items.deleteAttributeAt(0);
+        //医生选择的
+//        Instances allitems=db.getData("select * from knn where eye=0");
+//        Instances items=db.getData("select * from knn where eye=0");
+        Instances allitems=db.getData("select * from old where eye='OD'");
+        Instances items=db.getData("select * from old where eye='OD'");
+        items.deleteAttributeAt(items.numAttributes()-1);
+        items.deleteAttributeAt(7);
+        items.deleteAttributeAt(6);
+        items.deleteAttributeAt(5);
+        items.deleteAttributeAt(3);
+        items.deleteAttributeAt(1);
+        items.deleteAttributeAt(0);
         //属性归一化
         Normalize normalize = new Normalize();
         normalize.setInputFormat(items);
         items= Filter.useFilter(items, normalize);
-        //删除无用属性
-        for(int i=18;i<42;i++){
-            items.deleteAttributeAt(items.numAttributes()-1);
-        }
-        items.deleteAttributeAt(5);
-        items.deleteAttributeAt(2);
-        items.deleteAttributeAt(1);
-        items.deleteAttributeAt(0);
+
         ArrayList<Pair<Instance,Pair<Instance,Double>>> list=new ArrayList<>();
-        Instance temp=items.instance(0);
         //计算距离bing保存到output_string中
         StringBuffer output_string=new StringBuffer();
         String title="";
@@ -44,16 +58,20 @@ public class main {
             list.add(new Pair(allitems.instance(i),new Pair(items.instance(i),distance(items.instance(j),items.instance(i)))));
         }
         sort(list);
-            output_string.append(list.get(0).getKey()+",,"+list.get(0).getValue().getValue()+"\n");
+            output_string.append(list.get(0).getKey()+",,,"+list.get(0).getValue().getValue()+"\n");
             output_string.append("Rank:\n");
         for(int i=1;i<11;i++){
-            output_string.append(list.get(i).getKey()+",,"+list.get(i).getValue().getValue()+"\n");
+            output_string.append(list.get(i).getKey()+",,,"+list.get(i).getValue().getValue()+"\n");
         }
             output_string.append("\n");
         }
-        System.out.println(output_string.toString());
-    }
-    public void write(Instances data){
+
+
+        //文件输出
+        File file1 = new File("E:\\右眼_老数据_all.csv");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file1));
+        bw.write(output_string.toString());
+        bw.close();
 
     }
     public static double distance(Instance x,Instance y){
