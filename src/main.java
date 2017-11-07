50dd194b32006acfc3476abd7560fddf030f7501
mobai <381473354@qq.com>
2017-11-07 13:44:22 +0800

    add dev feature to support smote

--- src/main.java
+++ src/main.java
@@ -8,16 +8,20 @@ import java.util.ArrayList;
  * Created by 38147 on 2017/10/31.
  */
 public class main {
+    private static Instances allitems;
+    private static Instances items;
     public static void main(String args[]) throws Exception {
         DB db=new DB("jdbc:mysql://localhost:3306/eye","root","zch123");
         int[] deletepoint={5,4,2,1,0};
-        String sql="select * from nomogram where eye=1";
+        String sql="select * from nomogram";
+        allitems=db.getData(sql);
+        items=db.getData(sql);
+        Instance newinst=new Instance(2);
+        newinst.setDataset(items);
+        newinst.setValue(0,1);
+      //  newinst.setValue(1,2);
+     //   newinst.setValue(2,"shanghai");
 
-        Instances allitems=db.getData(sql);
-        Instances items=db.getData(sql);
-        Instance newinst=items.instance(0);
-        //newinst.setValue(2,"zhengchenghao");
-        newinst.setValue(1,940625);
         System.out.println(newinst);
 //
 //        //删除LeadEye之后的属性
@@ -47,10 +51,12 @@ public class main {
 //    private static HashMap<> Classify(int){
 //
 //    }
+    //计算新的属性      smote 算法
     private static Instance GetNewInstance(Instance instance,Instance neighbor){  //计算新的数据
         Instance newinstance=new Instance(instance.numAttributes());  //创建一个长度为
-        for(int i=0;i<instance.numAttributes();i++){
 
+        for(int i=0;i<instance.numAttributes();i++){
+            newinstance.setValue();
         }
     //规范化眼别，性别
     return newinstance;
